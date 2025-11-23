e12138f9b183d65f04cc1997a197c044aef1b274
KaheePark <mydnjang@naver.com>
2025-11-23 20:57:55 +0900

    출력 메세지 언더바에서 하이픈으로 변경

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     public static void printStatus(Car car){
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<car.getPosition(); i++){
-            sb.append("_");
+            sb.append("-");
         }
         System.out.println(car.getName()+ " : "+ sb);
     }
